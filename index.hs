f5f37da9bd0cb10004f4a3565bb0e2d96f88edac
luanbrito7 <lab7@ug3c50.windows.cin.ufpe.br>
2019-06-06 09:06:23 -0300

    print map lines works

--- index.hs
+++ index.hs
@@ -3,8 +3,6 @@ module Index where
 import Control.Concurrent
 import Control.Concurrent.MVar
 
-import Text.Printf
-
 -- horizontal :: () -> [String]
 -- horizontal _ = ["|", " ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 
@@ -14,32 +12,39 @@ import Text.Printf
 
 
 main = do
-    show (topBoundary 15 [])
-    show (bottomBoundary 15 15 [])
+    printLine $ topBoundary 15 []
+    printLine $ topBoundary 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ middleSpace 15 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ regularSpace 15 15 []
+    printLine $ bottomBoundary 15 15 []
     -- map <- newEmptyMVar
 
 topBoundary :: Int -> [Char] -> [Char]
 topBoundary 0 mapa = mapa ++ "\n"
-topBoundary num mapa = topBoundary (num-1) mapa ++ "="
+topBoundary num mapa = topBoundary (num-1) mapa ++ "_"
 
 bottomBoundary :: Int -> Int -> [Char] -> [Char]
 bottomBoundary num max mapa
-    | num == 0   = mapa ++ "|" ++ "\n"
+    | num == 1   = mapa ++ "|" ++ "\n"
     | num == max = bottomBoundary (num-1) max (mapa ++ "|")
-    | otherwise  = bottomBoundary (num-1) max (mapa ++ "=")
+    | otherwise  = bottomBoundary (num-1) max (mapa ++ "_")
 
--- middleSpace :: Int -> Int -> [Char] -> [Char]
--- middleSpace num max mapa =
---     | num == 0   = mapa ++ "|"
---     | num == max = middleSpace (num-1) max (mapa ++ "|")
---     | otherwise  = middleSpace (num-1) max (mapa ++ "_")
+middleSpace :: Int -> Int -> [Char] -> [Char]
+middleSpace num max mapa
+    | num == 1   = mapa ++ "|"
+    | num == max = middleSpace (num-1) max (mapa ++ "|")
+    | otherwise  = middleSpace (num-1) max (mapa ++ "-")
 
--- bottomBoundary :: Int -> Int -> [Char] -> [Char]
--- bottomBoundary num max mapa =
---     | num == 0   = mapa ++ "|"
---     | num == max = bottomBoundary (num-1) max (mapa ++ "|")
---     | otherwise  = bottomBoundary (num-1) max (mapa ++ "_")
-    
-dummyFunction :: Int -> [Char] -> [Char]
-dummyFunction 0 str = str
-dummyFunction n str = dummyFunction (n-1) (str ++ ".")
+regularSpace :: Int -> Int -> [Char] -> [Char]
+regularSpace num max mapa
+    | num == 1   = mapa ++ "|"
+    | num == max = regularSpace (num-1) max (mapa ++ "|")
+    | otherwise  = regularSpace (num-1) max (mapa ++ " ")
+
+printLine :: [Char] -> IO () -- Essa função vai receber cada linha do MVAR recursivamente e ir printando.
+printLine str = putStrLn (str)
